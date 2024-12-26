2fc522fb62ecbb4f0d7c74593a9f6fb328d1cdfc
ibrahimghellab <ghellabibrahim@gmail.com>
2024-12-26 23:02:21 +0100

    Add NavBar component to App layout for improved navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-
+import NavBar from './components/NavBar'
 import Product from './components/Product'
 function App() {
   return (
     <>
-    <div className='flex flex-row gap-5' >
-      <Product></Product>
-      <Product></Product>
-      <Product></Product>
-      <Product></Product>
-      <Product></Product>
-      <Product></Product>
-    </div>
-    
-    <h1>App</h1>
+      <NavBar></NavBar>
+      <div className='flex flex-row gap-5' >
+        <Product></Product>
+        <Product></Product>
+        <Product></Product>
+        <Product></Product>
+        <Product></Product>
+        <Product></Product>
+      </div>
+      
+      <h1>App</h1>
     </>
   )
 }
