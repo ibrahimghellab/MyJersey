81e405b1583f9450b90984573a3d5c0a8a7d4070
ibrahimghellab <ghellabibrahim@gmail.com>
2024-12-26 23:02:17 +0100

    Add custom width utility '12/13' to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '12/13': '97%',
+      }
+    },
   },
   plugins: [daisyui],
 }
