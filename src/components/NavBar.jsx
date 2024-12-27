9e540b213da362db815d587886130d952c7a3e4d
Kevinsouriya DEVANAD <devasouriya@gmail.com>
2024-12-27 19:14:47 +0100

    modified font vabar size

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,13 @@
 export default function NavBar() {
   return (
     <>
-        <nav className="navbar bg-black rounded-full m-auto mt-6 w-12/13 place-content-center h-11 ">
+        <nav className="navbar bg-black rounded-full m-auto mt-6 w-12/13 place-content-center h-16 ">
             <div className="flex-1 navbar-start">
                 <a className="px-2" href=""><img className="w-16 h-16" src="src/public/MyJersey_logo.png" alt="" /></a>
-                <a className="px-2 text-2xl" href="">Home</a>
-                <a className="px-2 text-2xl" href="">Collections</a>
-                <a className="px-2 text-2xl" href="">About us</a>
-                <a className="px-2 text-2xl" href="">Contact</a>
+                <a className="px-2 text-xl" href="">Home</a>
+                <a className="px-2 text-xl" href="">Collections</a>
+                <a className="px-2 text-xl" href="">About us</a>
+                <a className="px-2 text-xl" href="">Contact</a>
             </div>
             <div className="navbar-end">
                 <div className="px-2 ">
