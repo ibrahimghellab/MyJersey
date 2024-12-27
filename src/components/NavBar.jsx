d95a8bda857e404076537f85a3ff9007cd5c7439
ibrahimghellab <ghellabibrahim@gmail.com>
2024-12-27 19:07:34 +0100

    Update NavBar and Product components for improved layout and styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,9 @@
 export default function NavBar() {
   return (
     <>
-        <nav className="navbar bg-black rounded-full m-auto mt-6 w-12/13 place-content-center">
+        <nav className="navbar bg-black rounded-full m-auto mt-6 w-12/13 place-content-center h-11 ">
             <div className="flex-1 navbar-start">
-                <a className="px-2" href=""><img className="w-24 h-24" src="src/public/MyJersey_logo.png" alt="" /></a>
+                <a className="px-2" href=""><img className="w-16 h-16" src="src/public/MyJersey_logo.png" alt="" /></a>
                 <a className="px-2 text-2xl" href="">Home</a>
                 <a className="px-2 text-2xl" href="">Collections</a>
                 <a className="px-2 text-2xl" href="">About us</a>
@@ -12,13 +12,13 @@ export default function NavBar() {
             <div className="navbar-end">
                 <div className="px-2 ">
                     <label className="input input-bordered flex items-center gap-2">
-                        <img className="h-4 w-4 opacity-70" src="src/public/search.png" alt="" />
+                        <img className="h-3 w-3 opacity-70" src="src/public/search.png" alt="" />
                         <input type="text" placeholder="Search..."/>
                         
                     </label>
                 </div>
-                <a className="px-1" href=""><img className="w-11 h-11" src="src/public/shopping-cart.png" alt="" /></a>
-                <a className="px-1" href=""><img className="w-11 h-11" src="src/public/account.png" alt="" /></a>
+                <a className="px-1" href=""><img className="w-9 h-9" src="src/public/shopping-cart.png" alt="" /></a>
+                <a className="px-1" href=""><img className="w-9 h-9" src="src/public/account.png" alt="" /></a>
             </div>
         </nav>
     </>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,7 @@
 export default function Product() {
   return (
     <>
-      <div className="card card-bordered bg-base-100 w-60 shadow-xl">
+      <div className="card card-bordered w-60 shadow-xl">
         <figure>
           <img src="" alt="Image" />
         </figure>
