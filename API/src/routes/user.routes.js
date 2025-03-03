def698b907634511d5c748c964b8aa23976d14ec
ibrahimghellab <ghellabibrahim@gmail.com>
2025-03-03 23:27:33 +0100

    add route for user creation

--- API/src/routes/user.routes.js
+++ API/src/routes/user.routes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 
 router.get('/', userController.getAllUsers);
-
+router.post('/',userController.createUser)
 module.exports = router;
