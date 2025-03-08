db33a03165f962ba9ecd351d9a781d1a767c8b28
ibrahimghellab <ghellabibrahim@gmail.com>
2025-03-08 12:37:55 +0100

    add route for collections

--- API/src/app.js
+++ API/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/user.routes');
+const collectionRoutes = require('./routes/collection.routes')
 
 const app = express();
 app.use(cors());
@@ -8,5 +9,6 @@ app.use(express.json());
 
 // Routes
 app.use('/users', userRoutes);
+app.use('/collections', collectionRoutes)
 
 module.exports = app;
