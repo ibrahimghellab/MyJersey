5b83cc89298e7e102638eca5ac2fcfb971754e2c
ibrahimghellab <ghellabibrahim@gmail.com>
2025-03-08 12:37:58 +0100

    add collection controller with CRUD operations

---
+++ API/src/controllers/collection.controller.js
@@ -0,0 +1,44 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient()
+
+exports.getAllCollections = async(req,res) => {
+    try {
+        const collections = await prisma.collection.findMany();
+        res.json(collections);
+    } catch (error) {
+        res.status(500).json({ error: "Erreur serveur" });
+    }
+}
+
+exports.createCollection = async (req, res) => {
+    try {
+        const {name} = req.body;
+
+        const collection = await prisma.collection.create({
+            data: {
+                name : name
+            },
+        });
+
+        res.status(201).json({ message: 'Collection créé avec succès', collection });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erreur serveur' });
+    }
+};
+
+exports.getCollectionById = async (req,res) => {
+    try{
+        const id = parseInt(req.params.id);
+        const collection = await prisma.collection.findUnique({
+            where : {id_collection : id}
+        })
+        res.json(collection)
+    }catch(error){
+        console.error(error);
+        res.status(500).json({ error: 'Erreur serveur' });
+    }
+    
+
+}
