1a4b8653e98d974e41a99eaff8d0590ef2075a75
ibrahimghellab <ghellabibrahim@gmail.com>
2025-03-09 01:32:16 +0100

    add routes for orders and products in app.js

--- API/src/app.js
+++ API/src/app.js
@@ -2,13 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/user.routes');
 const collectionRoutes = require('./routes/collection.routes')
+const orderRoutes = require('./routes/order.routes')
+const productRoutes = require('./routes/product.routes')
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/users', userRoutes);
-app.use('/collections', collectionRoutes)
+app.use('/collections', collectionRoutes);
+app.use('/orders',orderRoutes);
+app.use('/products',productRoutes)
+
 
 module.exports = app;
