dd85f8873427632140bee78c76e8f7eb015d781f
ibrahimghellab <ghellabibrahim@gmail.com>
2025-03-09 01:32:41 +0100

    add route to retrieve orders by user ID in user controller

--- API/src/routes/user.routes.js
+++ API/src/routes/user.routes.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 
 router.get('/', userController.getAllUsers);
-router.post('/',userController.createUser)
-router.get('/:id',userController.getUserById)
+router.get('/:id',userController.getUserById);
+router.get('/:id/orders',userController.getOrdersByUserId);
+
+
+router.post('/',userController.createUser);
+
 module.exports = router;
 
